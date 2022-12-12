b4b1f92e79b414600bc60ff9c79ef9f40123737d
Murillo Vinícius <Murillovinicius13@hotmail.com>
2022-12-12 19:09:18 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Day_7
+ Curso 30DaysOfJavaScript, nesse dia aprendi sobre função e seus tipos de utilidades.
---
+++ fundamentos_day7.js
@@ -0,0 +1,16 @@
+function nomecompleto(){
+    const firstName = 'Murillo'
+    const secondName = 'Vinícius'
+    const space = ' '
+    const fullName = firstName + space + secondName
+    return fullName
+} // exemplo de função com return.
+console.log(nomecompleto())
+
+function areaDoCirculo(r){
+    let area = Math.PI * r * r
+    return area
+} // adicionando um paramentro na função
+console.log(areaDoCirculo(15))
+
+
